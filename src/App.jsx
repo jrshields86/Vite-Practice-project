911c03e7d6ad24762501acf60b112c01d4801c99
jrshields86 <jrshields86@gmail.com>
2023-09-05 15:46:49 -0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList)
+console.log(puppies)
   return (
     <>
       <div>
-        
+        {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>
+        })  
+        }
       </div>
-    
+      
     </>
   )
 }
